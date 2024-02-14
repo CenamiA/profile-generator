5781234ff000dfa5a157108532d7ca28d2ae88ff
Nami <cenami@live.fr>
2024-02-14 00:34:04 +0000

    updated set of questions

--- index.js
+++ index.js
@@ -13,3 +13,37 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const newTeamMember= [];
+
+const setQuestions = async() =>{
+    const answers = await inquirer
+    .prompt([
+
+{
+    type: "input",
+    message: "Hello, what's your name?",
+    name: "name",
+},
+
+{
+    type: "input",
+    message: "Please enter your ID number",
+    name: "id",
+},
+
+{
+    type: "input",
+    message: "Please provide your email address",
+    name: "email",
+},
+
+{
+    type: "list",
+    message: "What is your role?",
+    name: "role",
+    choices: ["Engineer", "Intern", "manager"]
+},
+
+
+    ])
+}
