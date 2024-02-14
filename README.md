919a8b40e9d4531bd24642ef4291e71826999144
Nami <cenami@live.fr>
2024-02-14 02:22:33 +0000

    genarated html file and updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # profile-generator
 Team Profile Generator
+## Description
+
+Generating a HTML file to create a team using CLI
+
+## Installation
+
+node index.js to run the questions in the CLI and open in the browser
+
+## Usage
+
+![alt text](<assets/Team Generator.png>)
+
+## Credits
+N/A
+
+## License
+
+Please refer to the license in the repo
--- index.js
+++ index.js
@@ -15,8 +15,9 @@ const render = require("./src/page-template.js");
 
 const newTeamMember= [];
 
+//Set of questions asked 
 const setQuestions = async() =>{
-    const answers = await inquirer
+    const userAnswers = await inquirer
     .prompt([
 
 {
@@ -46,4 +47,89 @@ const setQuestions = async() =>{
 
 
     ])
+
+if (userAnswers.role === "Manager" ){
+    const managerProfile = await inquirer.prompt([
+
+        {
+            type: "input",
+            message: "Please provide your office number",
+            name: "officeNumber",
+        
+        },
+    ])
+
+    const newManager = new Manager (
+        userAnswers.name, userAnswers.id, userAnswers.email, managerProfile.officeNumber
+        );
+        newTeamMember.push(newManager);
+
+} else if (userAnswers.role === "Engineer" ){
+    const engineerProfile = await inquirer.prompt([
+
+        {
+            type: "input",
+            message: "Please enter your Github user name",
+            name: "github",
+        
+        },
+    ])
+
+    const newEngineer = new Engineer (
+        userAnswers.name, userAnswers.id, userAnswers.email, engineerProfile.github
+        );
+        newTeamMember.push(newEngineer);
+
+}else if (userAnswers.role === "Intern"){
+    const internProfile = await inquirer.prompt([
+        {
+            type: "input",
+            message: "What university did you attend?",
+            name: "school",
+        
+        },
+
+    ])
+
+    const newIntern = new Intern (
+        userAnswers.name, userAnswers.id, userAnswers.email, internProfile.school
+    );
+    newTeamMember.push(newIntern);
+}
+
+};
+
+
+async function promptQuestions(){
+    await setQuestions()
+
+    const buildTeam = await inquirer.prompt([
+
+        {
+            name: "addTeamMember",
+            type: "list",
+            choices: ["Add a new team member", "Create a team", "Registration complete"]
+        }
+    ])
+    
+    if (buildTeam.addTeamMember === "Add a new team member"){
+        return promptQuestions()
+    } else if(buildTeam.addTeamMember === "Registration complete"){
+        return renderHTML();
+    }
+
+}
+
+promptQuestions();
+
+//function to generate html file
+
+renderHTML = function(){
+    const htmlContent = render(newTeamMember);
+
+    if(!fs.existsSync(OUTPUT_DIR)){
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, htmlContent);
+    console.log("Succes, your team has been created!");
 }
